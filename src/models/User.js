7ef1d9407817c7ed8337821d5ff7cd16e45789a1
Rapizzaaa <165604764+mvr2004@users.noreply.github.com>
2024-07-13 17:45:53 +0100

    Add files via upload

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../configs/database');
 const bcrypt = require('bcrypt');
-
+const Centro = require('./Centro');
 
 const User = sequelize.define('User', {
   id: {
